7a475c9c071becaa4f1bb021f4a5d6f9070fbc15
lipsapraharaj <69561010+lipsapraharaj@users.noreply.github.com>
2020-09-26 07:03:38 -0400

    Add files via upload

---
+++ File Details(Practical 4-New Guidelines)/FileDetails.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argv, char* argc[])
+{
+	if(argv==2)
+	{
+		char cmd[]="stat";
+		strcat(cmd,argc[1]);
+		system(cmd);
+		return 0;
+	}
+	else
+	{
+		std::cout<<"Incorrect Input\n";
+		return -1;
+	}
+}
